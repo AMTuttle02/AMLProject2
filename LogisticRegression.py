58b0f552641b9e96e18f81f2710f0432d842a7cb
Alexander Tuttle <amt262@uakron.edu>
2022-03-28 21:08:27 -0400

    Update comments

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-###################################################################
-# Separating fruits
-###################################################################
+# read heart disease data
 heart = pd.read_csv('heart_cleveland_upload.csv')
 
 heart.head()
 
+# X signifies attributes
+# y signifies 
 X = heart[['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']]
 y = heart['condition']
 
@@ -19,12 +19,10 @@ from sklearn.linear_model import LogisticRegression
 lr = LogisticRegression(random_state=40, max_iter=10000)
 lr.fit(X_train, y_train)
 
-#########################################
 # Coefficients of linear model (b_1,b_2,...,b_p): log(p/(1-p)) = b0+b_1x_1+b_2x_2+...+b_px_p
 print("lr.coef_: {}".format(lr.coef_))
 print("lr.intercept_: {}".format(lr.intercept_))
 
 # Estimate the accuracy of the classifier on future data, using the test data
-##########################################################################################
 print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
 print("Test set score: {:.2f}".format(lr.score(X_test, y_test)))
